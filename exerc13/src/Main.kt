7d0b0b1deaf70840e1d1c68e8a7d9606edb0b5e1
gabriela <125558278+gabilujan@users.noreply.github.com>
2024-02-23 19:24:10 -0300

    Add files via upload

---
+++ exerc13/src/Main.kt
@@ -0,0 +1,11 @@
+package fundamentos.controle
+fun main(args: Array<String>) {
+    val nota = 4
+    when (nota) {
+        10, 9 -> print("Fantástico")
+        8, 7 -> print("Parabéns")
+        6,5,4 -> print("Tem como recuperar")
+        in 3 .. 0 -> print("Nota inválida")
+
+    }
+}
