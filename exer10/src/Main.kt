7face4fa279d731d9895f9d102e6b1f2b0e61c9a
gabriela <125558278+gabilujan@users.noreply.github.com>
2024-02-23 17:59:27 -0300

    Delete exer10 directory

--- exer10/src/Main.kt
+++
@@ -1,9 +0,0 @@
-package fundamentos
-fun main(args:Array<String>){
-    val nota: Double = 6.9
-    if(nota  >= 7.0){
-        println("Aprovado!!")
-    }else{
-        println("Reprovado!!")
-    }
-}
